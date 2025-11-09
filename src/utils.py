d84bea67cd56fb11b593b55d759c5c4ce4d3c35c
angsongyee <angsongyee@gmail.com>
2025-11-09 17:35:59 +0800

    Add new notebook for training

---
+++ src/utils.py
@@ -0,0 +1,35 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import random
+
+def get_rand_img_path():
+    train_dir = os.getcwd() + "/../train/"
+    train_files = os.listdir(train_dir)
+    return train_dir + random.choice(train_files)
+
+def get_img(label):
+    train_dir = os.getcwd() + "/../train/"
+    path = os.path.join(train_dir,f"{label}-0.png")
+    img = cv2.imread(path)
+    return img
+
+def get_label(img_path):
+    filename = os.path.basename(img_path)
+    label = filename.split('-')[0]
+    return label
+
+def get_rand_img():
+    path = get_rand_img_path()
+    label = get_label(path)
+    img = cv2.imread(path)
+    return img, label
+
+def show_img(img, title=None):
+    plt.figure()
+    plt.axis("off")
+    if title is not None:
+        plt.title(title)
+    plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    plt.show()
